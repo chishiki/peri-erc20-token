eede1bc75af8e6be4457c54c9529ae2f54312cef
Christopher Webb <chishiki@gmail.com>
2021-11-24 12:47:28 +0900

    add metadata and a mint() function

--- contracts/peri_token.sol
+++ contracts/peri_token.sol
@@ -1,55 +1,105 @@
 // SPDX-License-Identifier: MIT
+// Inspired on token.sol from DappHub
+
 pragma solidity ^0.8.3;
+import "./IERC20Metadata.sol";
 
-import "./IERC20.sol";
+// We are using the built-in solidity overflow protection, but for underflow we are not, so that we can use custom error messages.
 
-contract PERI is IERC20 {
-    
-    uint public override totalSupply = 100000000;
-    
-    mapping(address => uint) public override balanceOf;
-    mapping(address => mapping(address => uint)) public override allowance;
-    
-    string public name = "PERIHELION TOKEN";
-    string public symbol = "PERI";
-    uint8 public decimals = 18;
+contract ERC20 is IERC20Metadata {
 
-    constructor() {
-        
-        balanceOf[msg.sender] = totalSupply;
-        
+    uint256                                           internal  _totalSupply;
+    mapping (address => uint256)                      internal  _balanceOf;
+    mapping (address => mapping (address => uint256)) internal  _allowance;
+    string                                            public override name = "PERIHELION TEST TOKEN";
+    string                                            public override symbol = "PER1";
+    uint8                                             public override decimals = 18;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) {
+        name = name_;
+        symbol = symbol_;
+        decimals = decimals_;
     }
 
-    function transfer(address recipient, uint amount) external override returns (bool) {
-        
-        balanceOf[msg.sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(msg.sender, recipient, amount);
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address guy) public view virtual override returns (uint256) {
+        return _balanceOf[guy];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return _allowance[owner][spender];
+    }
+
+    function approve(address spender, uint wad) public virtual override returns (bool) {
+        return _approve(msg.sender, spender, wad);
+    }
+
+    function transfer(address dst, uint wad) public virtual override returns (bool) {
+        return _transfer(msg.sender, dst, wad);
+    }
+
+    function transferFrom(address src, address dst, uint wad) public virtual override returns (bool) {
+        _decreaseApproval(src, wad);
+
+        return _transfer(src, dst, wad);
+    }
+
+    // Decrease approval if src != msg.sender and if not set to MAX
+    function _decreaseApproval(address src, uint wad) internal virtual returns (bool) {
+        if (src != msg.sender) {
+            uint256 allowed = _allowance[src][msg.sender];
+            if (allowed != type(uint).max) {
+                require(allowed >= wad, "ERC20: Insufficient approval");
+                unchecked { _approve(src, msg.sender, allowed - wad); }
+            }
+        }
+
         return true;
-        
     }
 
-    function approve(address spender, uint amount) external override returns (bool) {
+    function _transfer(address src, address dst, uint wad) internal virtual returns (bool) {
         
-        allowance[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
+        require(_balanceOf[src] >= wad, "ERC20: Insufficient balance");
+        unchecked { _balanceOf[src] = _balanceOf[src] - wad; }
+        _balanceOf[dst] = _balanceOf[dst] + wad;
+
+        emit Transfer(src, dst, wad);
+
         return true;
-        
     }
 
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) external override returns (bool) {
-        
-        allowance[sender][msg.sender] -= amount;
-        balanceOf[sender] -= amount;
-        balanceOf[recipient] += amount;
-        
-        emit Transfer(sender, recipient, amount);
+    function _approve(address owner, address spender, uint wad) internal virtual returns (bool) {
+        _allowance[owner][spender] = wad;
+        emit Approval(owner, spender, wad);
+
         return true;
-        
     }
-    
+
+    function _mint(address dst, uint wad) internal virtual returns (bool) {
+        _balanceOf[dst] = _balanceOf[dst] + wad;
+        _totalSupply = _totalSupply + wad;
+        emit Transfer(address(0), dst, wad);
+
+        return true;
+    }
+
+    function _burn(address src, uint wad) internal virtual returns (bool) {
+        unchecked {
+            require(_balanceOf[src] >= wad, "ERC20: Insufficient balance");
+            _balanceOf[src] = _balanceOf[src] - wad;
+            _totalSupply = _totalSupply - wad;
+            emit Transfer(src, address(0), wad);
+        }
+
+        return true;
+    }
+
+    // public mint function that exposes _mint()
+    function mint(address recipient, uint amount) public {
+        _mint(recipient, amount);
+    }
+
 }
